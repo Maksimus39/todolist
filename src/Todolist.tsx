02829d50589e412e9b66893f8bf8a472f112a1bf
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-12-07 20:40:45 +0300

    Спринт 01, неделя 1

---
+++ src/Todolist.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {Button} from "./Button";
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+export type PropsType = {
+    title: string
+    tasks: TaskType[]
+}
+
+export const Todolist = ({title,tasks}: PropsType) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            {tasks.length === 0 ? (
+                <p>Тасок нет</p>
+            ) : (
+                <ul>
+                    {tasks.map(task => {
+                        return (
+                            <li key={task.id}>
+                                <input type="checkbox" checked={task.isDone} />
+                                <span>{task.title}</span>
+                            </li>
+                        )
+                    })}
+                </ul>
+            )}
+            <div>
+               <Button title={'All'}/>
+               <Button title={'Active'}/>
+               <Button title={'Completed'}/>
+            </div>
+        </div>
+    )
+}
