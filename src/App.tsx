443f17b5b323fcdf4476bde177dfae7cbffd1866
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-12-31 14:27:27 +0300

    01 - Todolist for students

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
+
+    const tasks1 = [
+        {id: 1, title: 'CSS', isDone: true},
+        {id: 1, title: 'JS', isDone: true},
+        {id: 1, title: 'React', isDone: true},
+    ]
+
+    const tasks2 = [
+        {id: 1, title: 'Terminator', isDone: true},
+        {id: 1, title: 'XXX', isDone: true},
+        {id: 1, title: 'Robokop', isDone: true},
+    ]
+
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                    <button>master-Dimych</button>
-                </div>
-            </div>
+            <Todolist title={'What to learn'} tasks={tasks1}/>
+            <Todolist title={'What to buy'} tasks={tasks2}/>
         </div>
     );
 }
