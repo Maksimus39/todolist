ca2ebb6577eb99ee02c66410dc15a72515f072e0
Max <maksim.minakov.83@mail.ru>
2025-05-28 22:25:33 +0300

    sprint-4, lesson-3

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -1,11 +1,11 @@
-import type { RequestStatus } from '@/common/types';
-import { createSlice } from '@reduxjs/toolkit';
+import type { RequestStatus } from "@/common/types"
+import { createSlice } from "@reduxjs/toolkit"
 
 export const appSlice = createSlice({
-  name: 'app',
+  name: "app",
   initialState: {
-    themeMode: 'dark' as ThemeMode,
-    status: 'idle' as RequestStatus,
+    themeMode: "light" as ThemeMode,
+    status: "idle" as RequestStatus,
     error: null as string | null,
   },
   selectors: {
@@ -15,19 +15,19 @@ export const appSlice = createSlice({
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
-      state.themeMode = action.payload.themeMode;
+      state.themeMode = action.payload.themeMode
     }),
     setAppStatusAC: create.reducer<{ status: RequestStatus }>((state, action) => {
-      state.status = action.payload.status;
+      state.status = action.payload.status
     }),
     setAppErrorAC: create.reducer<{ error: string | null }>((state, action) => {
-      state.error = action.payload.error;
+      state.error = action.payload.error
     }),
   }),
-});
+})
 
-export const { selectThemeMode, selectAppStatus, selectAppError } = appSlice.selectors;
-export const { changeThemeModeAC, setAppStatusAC, setAppErrorAC } = appSlice.actions;
-export const appReducer = appSlice.reducer;
+export const { selectThemeMode, selectAppStatus, selectAppError } = appSlice.selectors
+export const { changeThemeModeAC, setAppStatusAC, setAppErrorAC } = appSlice.actions
+export const appReducer = appSlice.reducer
 
-export type ThemeMode = 'dark' | 'light';
+export type ThemeMode = "dark" | "light"
