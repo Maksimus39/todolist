8d9ca407c6c6d9184fa4cb6f31b119cc5fa50d1b
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-07-08 23:27:26 -0700

    5-2

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -6,10 +6,7 @@ export const tasksApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     getTasks: build.query<GetTasksResponse, string>({
       query: (todolistId) => `todo-lists/${todolistId}/tasks`,
-      providesTags: (res, _err, todolistId) =>
-        res
-          ? [...res.items.map(({ id }) => ({ type: 'Task', id }) as const), { type: 'Task', id: todolistId }]
-          : ['Task'],
+      providesTags: (_res, _err, todolistId) => [{ type: "Task", id: todolistId }],
     }),
     addTask: build.mutation<BaseResponse<{ item: DomainTask }>, { todolistId: string; title: string }>({
       query: ({ todolistId, title }) => ({
@@ -24,7 +21,7 @@ export const tasksApi = baseApi.injectEndpoints({
         url: `todo-lists/${todolistId}/tasks/${taskId}`,
         method: 'DELETE',
       }),
-      invalidatesTags: (_res, _err, { taskId }) => [{ type: 'Task', id: taskId }],
+      invalidatesTags: (_res, _err, { todolistId }) => [{ type: 'Task', id: todolistId }],
     }),
     updateTask: build.mutation<
       BaseResponse<{ item: DomainTask }>,
@@ -35,7 +32,7 @@ export const tasksApi = baseApi.injectEndpoints({
         method: 'PUT',
         body: model,
       }),
-      invalidatesTags: (_res, _err, { taskId }) => [{ type: 'Task', id: taskId }],
+      invalidatesTags: (_res, _err, { todolistId }) => [{ type: 'Task', id: todolistId }],
     }),
   }),
 });
